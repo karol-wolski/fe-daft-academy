0bb89c2ffe58b7042134ce92ea877f6c9d2382e1
Karol Wolski <karolwolski04@gmail.com>
2022-05-11 00:21:52 +0200

    feat(app): add handler to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleShowAlert = () => alert('Hello React')
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleShowAlert}>Show alert</button>
       </header>
     </div>
   );
