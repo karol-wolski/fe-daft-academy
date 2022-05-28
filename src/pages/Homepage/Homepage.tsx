f87f2c909511394145d23ac023a90001d1832968
Karol Wolski <karolwolski04@gmail.com>
2022-05-28 15:33:16 +0200

    refactor(pages): remove duplicates 'title' class from different style modules

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -5,7 +5,7 @@ import styles from './Homepage.module.css'
 const Homepage = () => {
   return (
     <>
-      <h2 className={styles.title}>Welcome to the page</h2>
+      <h2 className="title">Welcome to the page</h2>
       <p className={styles.desc}>This page was created during the FrontEnd 4 Beginners course as one of the homework assignments.</p>
       <div className={styles.containerLogo}>
         <img src={logoAcademy} alt="" className={styles.logo} />
--- src/pages/Week2/Week2.tsx
+++ src/pages/Week2/Week2.tsx
@@ -1,10 +1,9 @@
 import Form from "../../components/Form/Form"
-import styles from './Week2.module.css'
 
 const Homepage = () => {
   return (
    <>
-    <h2 className={styles.title}>Labolatory week 2</h2>
+    <h2 className="title">Labolatory week 2</h2>
     <Form />
    </>
   )
